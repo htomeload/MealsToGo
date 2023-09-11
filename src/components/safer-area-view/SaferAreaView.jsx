69a10386010a0b6b13325b36f5955de5c6a10e8c
htomeload <ko_player_555@outlook.com>
2023-09-11 19:43:41 +0700

    Applied eslint and prettier

---
+++ src/components/safer-area-view/SaferAreaView.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView, StyleSheet, StatusBar, Platform,
+} from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+
+const Styles = StyleSheet.create({
+  container: {
+    paddingTop: Platform?.OS === 'android' ? StatusBar?.currentHeight : 0,
+  },
+});
+
+export default function SaferAreaView(props) {
+  return (
+    <>
+      {/* eslint-disable-next-line react/prop-types */}
+      <SafeAreaView style={[Styles.container, props?.style]}>
+        {/* eslint-disable-next-line react/prop-types */}
+        {props?.children}
+      </SafeAreaView>
+      {/* eslint-disable-next-line react/style-prop-object */}
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
